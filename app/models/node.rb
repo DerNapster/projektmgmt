17f4e98c83c303e78838e8d8699983c7f3d640b6
marco135 <mkuepers@gmx.de>
2016-01-06 20:04:21 +0100

    Self join Kack

--- app/models/node.rb
+++ app/models/node.rb
@@ -1,6 +1,6 @@
 class Node < ActiveRecord::Base
-  belongs_to :pbstable
+  belongs_to :pbstable, :class_name => 'Pbstable'
   belongs_to :parent, :class_name => 'Node'
-  has_one :children, :class_name => 'Node', :foreign_key => 'parent_id'
+  has_many :children, :class_name => 'Node', :foreign_key => 'parent_id'
   accepts_nested_attributes_for :parent
 end
--- app/models/pbstable.rb
+++ app/models/pbstable.rb
@@ -1,5 +1,5 @@
 class Pbstable < ActiveRecord::Base
   belongs_to :project
-  has_many :nodes
-  accepts_nested_attributes_for :nodes
+  has_one :node
+  accepts_nested_attributes_for :node
 end
