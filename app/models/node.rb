28e3d944852842b5a8b6acbc46011ba465c6b932
marco135 <mkuepers@gmx.de>
2016-01-07 18:33:09 +0100

    Model Validation

--- app/models/node.rb
+++ app/models/node.rb
@@ -3,4 +3,26 @@ class Node < ActiveRecord::Base
   belongs_to :parent, :class_name => 'Node'
   has_many :children, :class_name => 'Node', :foreign_key => 'parent_id'
   accepts_nested_attributes_for :parent
+  validates :name, presence: true
+  validates :pbstable, presence: true
+  validates :startdate, presence: true
+  validates :enddate, presence: true
+  validates :level, presence: true
+  validates :duration, presence: true
+  validates :milestone, presence: true
+  validates :enddate_must_be_greater_than_startdate
+  validates :startdate_plus_duration_must_be_enddate
+
+
+  def enddate_must_be_greater_than_startdate
+    if enddate < startdate
+      errors.add(:enddate, "Enddate can't be smaller than startdate")
+    end
+  end
+
+  def startdate_plus_duration_must_be_enddate
+    unless (startdate+duration) == enddate
+      errors.add(:enddate, "Startdate plus duration must be enddate")
+    end
+  end
 end
--- app/models/pbstable.rb
+++ app/models/pbstable.rb
@@ -2,4 +2,6 @@ class Pbstable < ActiveRecord::Base
   belongs_to :project
   has_many :nodes
   accepts_nested_attributes_for :nodes
+  validates :name, presence: true
+  validates :project, presence: true
 end
