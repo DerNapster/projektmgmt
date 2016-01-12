c049d8d1b3b3af5a31c0bb521c09b9bd12ea726b
mark-mit-k <warneke.mark@gmail.com>
2016-01-12 16:12:45 +0100

    rest erweitert

--- app/assets/javascripts/nodes-module.js
+++ app/assets/javascripts/nodes-module.js
@@ -10,12 +10,106 @@
 
   nodes.run ( function ($nodes, $log){
     $log.debug($nodes.getNodes());
-    $log.debug($nodes.getNode(1))
+
+
+    $log.debug($nodes.getNode({node_id:1}));
   });
 
 
+  ///
+  /// Datalogic
+  /// Provides Methods to consume the REST API
+  ///
+  nodes.controller ('nodesController', function ($scope, $nodes) {
+
+    /*
+    * GET /nodes.json
+    * @return Nodes List
+    */
+    $scope.nodes = $nodes.getNodes();
+
+    /*
+     * GET /nodes/{id}.json
+     * @return Node
+     */
+    $scope.getNode = function ( id ) {
+      $nodes.getNode( { node_id:id } );
+    };
+
+    /*
+     * POST /nodes
+     * @param name, description, level, duration, startdate, enddate, milestone, pbstable_id, parent_id
+     * @return created Node
+     */
+    $scope.newNode = function ( name, description, level, duration, startdate, enddate, milestone, pbstable_id, parent_id ) {
+      // $nodes.save(
+      $nodes.createNode(
+        {
+          name: name,
+          description: description,
+          level: level,
+          duration: duration,
+          startdate: startdate,
+          enddate: enddate,
+          milestone: milestone,
+          pbstable_id: pbstable_id,
+          parent_id: parent_id
+        }, function ( data )
+        {
+          $log.debug ( data );
+          // refresh nodes
+          $scope.nodes = $nodes.getNodes();
+        }
+      );
+    };
+
+    /*
+     * PUT /nodes
+     * @param Node Object
+     * @return updated Node
+     */
+    $scope.updateNode = function ( node ) {
+      $log.debug(node);
+      node.$updateNode ( function ( data ) {
+        $log.debug ( data );
+        // refresh nodes
+        $scope.nodes = $nodes.getNodes();
+      });
+    };
+
+    /*
+     * DELETE /nodes/{id}.json
+     * @param id of node
+     */
+    $scope.deletNodeById = function ( id ) {
+      $nodes.deleteUser ( { node_id:id }, function ( data ) {
+        $log.debug ( data );
+        // refresh nodes
+        $scope.nodes = $nodes.getNodes();
+      });
+    };
+
+    /*
+     * DELETE /nodes/{id}.json
+     * @param node
+     */
+    $scope.deletNode = function ( user ) {
+      $nodes.$remove ( function ( data ) {
+        $log.debug ( data );
+        // refresh nodes
+        $scope.nodes = $nodes.getNodes();
+      });
+    };
+
+  });
+
+  ///
+  /// REST API
+  /// Provides GET, POST, DELETE, PUT Methods
+  /// use getNode, getNodes, createNode, deleteNode, updateNode
+  ///
   nodes.provider('$nodes', function () {
-      var endpoint = '/nodes.json';
+      var endpoint = '/nodes';
 
       this.setEndpoint = function ( url ) {
         endpoint = url;
@@ -24,7 +118,7 @@
       this.$get = function ( $resource ) {
 
         return $resource (
-          endpoint + "/:node_id",
+          endpoint + "/:node_id" + ".json",
           {
             node_id:'@id'
           },
@@ -32,8 +126,8 @@
            'getNode': {method:'GET'},
            'createNode': {method:'POST'},
            'getNodes': {method:'GET', isArray:true},
-           'deletNode': {method: 'DELETE' },
-           'updateNode': {method: 'PUT', responseType: 'text'}
+           'deleteNode': {method: 'DELETE' },
+           'updateNode': {method: 'PUT', responseType: 'text'} //Rest delivers text not object (succes message)
           }
         );
       }
