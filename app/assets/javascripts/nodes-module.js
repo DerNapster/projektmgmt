d31e6b70c3730aa65068360a9b1574888692fc9d
mark-mit-k <warneke.mark@gmail.com>
2016-01-16 19:26:37 +0100

    adds add node to parent, adds add node to select parent

--- app/assets/javascripts/nodes-module.js
+++ app/assets/javascripts/nodes-module.js
@@ -15,6 +15,7 @@
 
 
     $log.debug($nodes.getNode({node_id:1}));
+
   });
 
 
@@ -22,7 +23,10 @@
   /// Datalogic
   /// Provides Methods to consume the REST API
   ///
-  nodes.controller ('nodesController', function ($scope, $nodes, $log, $mdToast, $mdDialog, $mdMedia) {
+  nodes.controller ('nodesController', function ($scope, $nodes, $log, $mdDialog, $mdMedia) {
+
+
+    $scope.parent;
 
     $scope.pbstable = function () {
       $scope.pbstable = $scope.nodes[0].pbstable_id;
@@ -63,7 +67,11 @@
         }, function ( data )
         {
           $log.debug ( data );
-          toasterControllerProvider.showSimpleToast ( data.name + 'erstellt!');
+
+          if (data.status == 500 ) {
+            $log.debug( '500');
+          }
+
           // refresh nodes
           $scope.nodes = $nodes.getNodes();
         }
@@ -108,26 +116,48 @@
       });
     };
 
-    $scope.addNodeDialog = function(ev, node) {
+    $scope.addNodeDialog = function(ev, node, nodes) {
+
+      if(node) {
+        $scope.parent = node.id;
+      }
+
       var useFullScreen = ($mdMedia('sm') || $mdMedia('xs'))  && $scope.customFullscreen;
       $mdDialog.show({
-        controller: DialogController,
+        controller: NodeDialogController,
         templateUrl: 'newNode.html',
         parent: angular.element(document.body),
         targetEvent: ev,
+        locals: {
+          node: node,
+          nodes: nodes
+        },
         clickOutsideToClose:true,
         fullscreen: useFullScreen
       })
       .then(function(answer) {
-        $scope.status = 'You said the information was "' + answer + '".';
+        // $scope.status = 'You said the information was "' + answer + '".';
+        $log.debug(answer);
+
+        if (answer.parent_id) {
+            $scope.newNode ( answer.name, answer.description, answer.level, answer.duration, answer.startdate, answer.enddate, answer.milestone, $scope.pbstable, answer.parent  )
+        } else {
+            $scope.newNode ( answer.name, answer.description, answer.level, answer.duration, answer.startdate, answer.enddate, answer.milestone, $scope.pbstable, $scope.parent  )
+            $scope.parent = "";
+        }
+
+
+
       }, function() {
         $scope.status = 'You cancelled the dialog.';
+        $scope.parent = "";
       });
       $scope.$watch(function() {
         return $mdMedia('xs') || $mdMedia('sm');
       }, function(wantsFullScreen) {
         $scope.customFullscreen = (wantsFullScreen === true);
       });
+
     };
 
     $scope.deleteNodeDialog = function(ev, node) {
@@ -136,8 +166,9 @@
               .title('Wollen Sie wirklich den Knoten ' + node.name + ' löschen?')
               .textContent('Es werden alle Kind Elemente des Knoten gelöscht')
               .targetEvent(ev)
-              .ok('Löschen')
               .cancel('Abbrechen')
+              .ok('Löschen')
+
         $mdDialog.show(confirm).then(function( ) {
           $scope.deleteNodeById( node.id );
           $scope.message = 'Knoten gelöscht';
@@ -178,20 +209,14 @@
       }
   });
 
-  function DialogController($scope, $mdDialog) {
-    $scope.hide = function() {
-      $mdDialog.hide();
-    };
+  function NodeDialogController($scope, $mdDialog, node, nodes) {
+    $scope.node = node;
+    $scope.nodes = nodes;
+
     $scope.cancel = function() {
       $mdDialog.cancel();
     };
-    $scope.answer = function( node ) {
-
-      newNode( node );
-
-      toasterControllerProvider.showSimpleToast ( node.name + 'erstellt!');
-
-
+    $scope.answer = function( answer ) {
       $mdDialog.hide(answer);
     };
   }
