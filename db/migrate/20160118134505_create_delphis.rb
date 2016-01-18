5c4d30986c36add0fe0e8c7f398904334ffc5847
marco135 <mkuepers@gmx.de>
2016-01-18 15:13:41 +0100

    Delphi

--- db/migrate/20160118134505_create_delphis.rb
+++ db/migrate/20160118134505_create_delphis.rb
@@ -3,10 +3,10 @@ class CreateDelphis < ActiveRecord::Migration
     create_table :delphis do |t|
       t.string :name
       t.references :workpackage, index: true, foreign_key: true
-      t.int :round
-      t.int :pessimistic
-      t.int :realistic
-      t.int :optimistic
+      t.integer :round
+      t.integer :pessimistic
+      t.integer :realistic
+      t.integer :optimistic
 
       t.timestamps null: false
     end
