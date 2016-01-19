23602d712f28b806c613be8c1cdcd50fc65c92ee
DerNapster <christopher.werning@gmail.com>
2016-01-19 13:54:31 +0100

    fixed db error

--- app/models/delphi.rb
+++ app/models/delphi.rb
@@ -2,7 +2,7 @@ class Delphi < ActiveRecord::Base
   belongs_to :workpackage
   accepts_nested_attributes_for :workpackage
   validates :name, presence: true
-  validates :name, uniqueness: true
+  #validates :name, uniqueness: true
   validates :workpackage, presence: true
   validates :value, presence: true
 
--- db/migrate/20160118134505_create_delphis.rb
+++ db/migrate/20160118134505_create_delphis.rb
@@ -1,7 +1,7 @@
 class CreateDelphis < ActiveRecord::Migration
   def change
     create_table :delphis do |t|
-      t.string :name, null: false, unique: true
+      t.string :name, null: false
       t.references :workpackage, index: true, foreign_key: true
       t.integer :value
 
